d582a85bea7ba472b66859af5573cc06f9306c53
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-03 01:30:12 +0800

    准备码表资源

--- tools/README.md
+++ tools/README.md
@@ -11,3 +11,6 @@ Ctrl+Shift+F、Ctrl+Shift+U、Ctrl+Shift+H、 Ctrl+Shift+K、Ctrl+Shift+J
 - 热键配置   https://github.com/yanhuacuo/98wubi
 - 样式参考 https://github.com/yanhuacuo/css/blob/master/cd.jpg
 - 关键字切换方式 https://gitee.com/wubi98/weasel-98wb/blob/master/ReadMe.md#关键字切换方式
+
+## 码表资源
+- https://github-com.proxy.zibenyulun.cn/yanhuacuo/98wubi-tables
--- tools/download-chromium-extension.sh
+++ tools/download-chromium-extension.sh
@@ -21,10 +21,29 @@ fi
 
 
 
-PROXY_URL=${2:+'http://127.0.0.1:8015'}
+# 使用代理下载源码
+#  bash tools/update-library.sh --proxy http://192.168.3.26:8015
 
-export http_proxy=$PROXY_URL
-export https_proxy=$PROXY_URL
+PROXY_URL=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    PROXY_URL="$2"
+    shift
+    ;;
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+if [ -n $PROXY_URL ] ; then
+  export http_proxy=$PROXY_URL
+  export https_proxy=$PROXY_URL
+  export no_proxy='0.0.0.0/8,10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,224.0.0.0/4,233.252.0.0/24,240.0.0.0/4,255.255.255.255/32,localhost,.npmmirror.com,.aliyuncs.com,.taobao.org,.tsinghua.edu.cn,.ustc.edu.cn,.aliyun.com'
+
+fi
 
 test -d temp && rm -rf temp
 mkdir -p temp
@@ -56,7 +75,4 @@ set -e
 cd ${__DIR__}/
 
 exit 0
-
-
-
 test -d temp && rm -rf temp
