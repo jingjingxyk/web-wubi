56427c0726ef698b5604bf4f55ab78acb4525c74
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-02 17:18:47 +0800

    准备查询单字

--- content-scripts/src/Component/input-box/input-method-ui.js
+++ content-scripts/src/Component/input-box/input-method-ui.js
@@ -1,8 +1,8 @@
 import { show_input_box_options}  from "./input-method-options.js"
 let show_input_box_ui=(key,code,keyword=[])=>{
-
-
-
+    //输入框 样式 参考 https://github.com/yanhuacuo/css/blob/master/01.png
+    //show_input_box_options()
+    //todo
 }
 export {
     show_input_box_ui
--- content-scripts/src/Component/input-box/input-method.js
+++ content-scripts/src/Component/input-box/input-method.js
@@ -7,7 +7,7 @@ let bindinputMethod = () => {
             let str =
                 "KeyboardEvent: key='" + event.key + "' | code='" + event.code + "'";
             console.log(str);
-            console.log(event.target, event.target.nodeType, event.target.nodeName);
+            //console.log(event.target, event.target.nodeType, event.target.nodeName);
 
             // 只处理 文本输入元素标签  input 和 textarea
             // 特殊情况： 比如可编辑模式 和 超级模式 ：https://www.cnblogs.com/jingjingxyk/p/16575750.html
@@ -18,8 +18,7 @@ let bindinputMethod = () => {
                 event.target.nodeName === "INPUT" ||
                 event.target.getAttribute("contenteditable") === true
             ) {
-                console.log("捕获 输入光标 位置 ； 展示输入选项框 和 输入候选项");
-
+                //console.log("捕获 输入光标 位置 ； 展示输入选项框 和 输入候选项");
                 input_method_core(event.key, event.code)
             }
 
