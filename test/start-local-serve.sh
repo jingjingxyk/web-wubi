2a57381ce29bbcc98721e6bd8894b02932c04aef
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-02 14:49:19 +0800

    update action

---
+++ test/start-local-serve.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -exu
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=$(
+  cd ${__DIR__}/../
+  pwd
+)
+cd ${__PROJECT__}
+
+cd ${__PROJECT__}/test
+python3 -m http.server 8000
